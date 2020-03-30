35e3f77162640cc522100700b6ada6c40471b158
Jay Sorg <jay.sorg@gmail.com>
2020-03-29 23:55:43 -0700

    add files

---
+++ Makefile
@@ -0,0 +1,26 @@
+
+YAMIPATH=/opt/yami
+BMSDKINCPATH=/home/jay/bbsdk10.9.9/Linux/include
+
+OBJS=bmd.o bmd_declink.o DeckLinkAPIDispatch.o
+# bmd_peer.o bmd_log.o bmd_utils.o
+
+CFLAGS=-O2 -g -Wall -Wextra -I$(YAMIPATH)/include
+
+CXXFLAGS=-O2 -g -Wall -Wextra -I$(BMSDKINCPATH) -I$(YAMIPATH)/include
+
+LDFLAGS=-L$(YAMIPATH)/lib -Wl,-rpath=$(YAMIPATH)/lib
+
+LIBS=-lyami_inf -lm -ldl -lpthread
+
+bmd: $(OBJS)
+	$(CXX) -o bmd $(OBJS) $(LDFLAGS) $(LIBS)
+
+clean:
+	rm -f $(OBJS) bmd
+
+bmd_declink.o: bmd_declink.cpp
+	$(CXX) $(CXXFLAGS) -c bmd_declink.cpp
+
+DeckLinkAPIDispatch.o: $(BMSDKINCPATH)/DeckLinkAPIDispatch.cpp
+	$(CXX) $(CXXFLAGS) -c $(BMSDKINCPATH)/DeckLinkAPIDispatch.cpp
---
+++ bmd_declink.cpp
@@ -0,0 +1,113 @@
+/**
+ * black magic daemon
+ *
+ * Copyright 2020 Jay Sorg <jay.sorg@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+
+#include <DeckLinkAPI.h>
+
+class DeckLinkCaptureDelegate : public IDeckLinkInputCallback
+{
+    public:
+        DeckLinkCaptureDelegate(void);
+        ~DeckLinkCaptureDelegate(void);
+        virtual HRESULT STDMETHODCALLTYPE QueryInterface(REFIID iid, LPVOID *ppv);
+        virtual ULONG STDMETHODCALLTYPE AddRef(void);
+        virtual ULONG STDMETHODCALLTYPE Release(void);
+        virtual HRESULT STDMETHODCALLTYPE VideoInputFormatChanged(BMDVideoInputFormatChangedEvents,
+                                                                  IDeckLinkDisplayMode*,
+                                                                  BMDDetectedVideoInputFormatFlags);
+        virtual HRESULT STDMETHODCALLTYPE VideoInputFrameArrived(IDeckLinkVideoInputFrame*,
+                                                                 IDeckLinkAudioInputPacket*);
+    private:
+        ULONG m_refCount;
+        pthread_mutex_t m_mutex;
+};
+
+/******************************************************************************/
+DeckLinkCaptureDelegate::DeckLinkCaptureDelegate(void)
+{
+    printf("DeckLinkCaptureDelegate:\n");
+    m_refCount = 0;
+    pthread_mutex_init(&m_mutex, NULL);
+}
+
+/******************************************************************************/
+DeckLinkCaptureDelegate::~DeckLinkCaptureDelegate(void)
+{
+    printf("~DeckLinkCaptureDelegate:\n");
+    pthread_mutex_destroy(&m_mutex);
+}
+
+/******************************************************************************/
+HRESULT DeckLinkCaptureDelegate::QueryInterface(REFIID iid, LPVOID *ppv)
+{
+    (void)iid;
+    (void)ppv;
+    printf("QueryInterface:\n");
+    return E_NOINTERFACE;
+}
+
+/******************************************************************************/
+ULONG DeckLinkCaptureDelegate::AddRef(void)
+{
+    printf("AddRef:\n");
+    pthread_mutex_lock(&m_mutex);
+    m_refCount++;
+    pthread_mutex_unlock(&m_mutex);
+    return (ULONG)m_refCount;
+}
+
+/******************************************************************************/
+ULONG DeckLinkCaptureDelegate::Release(void)
+{
+    printf("Release:\n");
+    pthread_mutex_lock(&m_mutex);
+    m_refCount--;
+    pthread_mutex_unlock(&m_mutex);
+    if (m_refCount == 0)
+    {
+        delete this;
+        return 0;
+    }
+    return (ULONG)m_refCount;
+}
+
+/******************************************************************************/
+HRESULT DeckLinkCaptureDelegate::VideoInputFormatChanged(BMDVideoInputFormatChangedEvents events,
+                                                         IDeckLinkDisplayMode *mode,
+                                                         BMDDetectedVideoInputFormatFlags flags)
+{
+    (void)events;
+    (void)mode;
+    (void)flags;
+    printf("VideoInputFormatChanged:\n");
+    return S_OK;
+}
+
+/******************************************************************************/
+HRESULT DeckLinkCaptureDelegate::VideoInputFrameArrived(IDeckLinkVideoInputFrame* videoFrame,
+                                                        IDeckLinkAudioInputPacket* audioFrame)
+{
+    (void)videoFrame;
+    (void)audioFrame;
+    printf("VideoInputFrameArrived:\n");
+    return S_OK;
+}
